625a213da21f79947ca398b5a6fdf448c13e8c91
Paulos-se <llcoolp@live.co.uk>
2022-07-27 08:36:21 +0100

    updated the like unlike button to like or unlike just once

--- src/components/UpdateVote.jsx
+++ src/components/UpdateVote.jsx
@@ -1,37 +1,50 @@
 import axios from "axios";
+import { useState } from "react";
 
 function UpdateVote({ vote, setVote, setArticle, article }) {
-  function upVote() {
-    axios
-      .patch(
-        `https://nc-news-pa.herokuapp.com/api/articles/${article.article_id}`,
-        { inc_votes: 1 }
-      )
-      .then((res) => {
-        setVote(res.data.votes);
-      });
+  const [liked, setLiked] = useState(false);
+  const [unLiked, setUnLiked] = useState(false);
+  function upVote(e) {
+    if (liked) {
+      e.preventDefault();
+    } else {
+      axios
+        .patch(
+          `https://nc-news-pa.herokuapp.com/api/articles/${article.article_id}`,
+          { inc_votes: 1 }
+        )
+        .then((res) => {
+          setVote(res.data.votes);
+        });
 
-    setVote((current) => {
-      let newVote = current + 1;
-      return newVote;
-    });
+      setVote((current) => {
+        let newVote = current + 1;
+        return newVote;
+      });
+      setLiked(true);
+    }
   }
 
-  function downVote() {
-    axios
-      .patch(
-        `https://nc-news-pa.herokuapp.com/api/articles/${article.article_id}`,
-        { inc_votes: -1 }
-      )
-      .then((res) => {
-        setVote(res.data.votes);
+  function downVote(e) {
+    if (unLiked) {
+      e.preventDefault();
+    } else {
+      axios
+        .patch(
+          `https://nc-news-pa.herokuapp.com/api/articles/${article.article_id}`,
+          { inc_votes: -1 }
+        )
+        .then((res) => {
+          setVote(res.data.votes);
+        });
+
+      setVote((current) => {
+        let newVote = current - 1;
+
+        return newVote;
       });
-
-    setVote((current) => {
-      let newVote = current - 1;
-
-      return newVote;
-    });
+      setUnLiked(true);
+    }
   }
 
   return (
